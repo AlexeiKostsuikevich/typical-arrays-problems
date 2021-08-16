57f923f8086484a968bcd9baddf8ce93cdd0cba7
AlexeiKostsuikevich <alexcompaq49@hotmail.com>
2021-08-16 20:30:22 +0300

    init: start task tupical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 
 exports.min = function min (array) {
+    if (array === undefined){
   return 0;
+    }
+    if (array.length === 0){
+        return 0;
+    }
+    return Math.min.apply(Math,array);
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array === undefined){
+  return 0;}
+  if (array.length === 0){
+      return 0;
+  }
+  return Math.max.apply(Math, array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
-}
+    if (array === undefined){
+        return 0;
+    }
+  if (array.length === 0){
+      return 0;
+  }
+  let divident = 0;
+  for (let i = 0; i < array.length; i++){
+      divident += array[i];
+  }
+  return divident/array.length;
+  }
+
